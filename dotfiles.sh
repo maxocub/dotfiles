3fc2b550f09dcbda15c83525a6c0b419c0cb2a4c
Maxime Turcotte <maxime.turcotte@erudit.org>
2024-01-05 19:22:13 -0500

    Use find to get the list of dotfiles to stow

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-dotfiles=$(echo "bin git i3 nvim tmux zsh")
+dotfiles=$(find . -mindepth 1 -maxdepth 1 -type d -not -path ./.git)
 
 for f in $dotfiles; do
-    stow --restow --target=$HOME $f
+    stow --restow --target=$HOME $(basename $f)
 done
