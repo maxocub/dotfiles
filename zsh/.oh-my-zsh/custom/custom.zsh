eb272059a0e8e1bac2fbead3a0cd98e37cca4b86
Maxime Turcotte <maxime.turcotte@erudit.org>
2024-01-24 15:16:47 -0500

    zsh: export catppuccin colors for fzf

--- zsh/.oh-my-zsh/custom/custom.zsh
+++ zsh/.oh-my-zsh/custom/custom.zsh
@@ -32,3 +32,8 @@ alias vi="nvim"
 
 # Ctrl+f to lunch tmux-sessionizer
 bindkey -s ^s "tmux-sessionizer\n"
+
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
