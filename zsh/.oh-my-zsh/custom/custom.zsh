614e44a27e70e4135fc43a4f3d87be22654688e0
Maxime Turcotte <maxime.turcotte@erudit.org>
2024-01-25 22:38:14 -0500

    zsh: add comment

--- zsh/.oh-my-zsh/custom/custom.zsh
+++ zsh/.oh-my-zsh/custom/custom.zsh
@@ -33,6 +33,7 @@ alias vi="nvim"
 # Ctrl+s to lunch tmux-sessionizer
 bindkey -s ^s "tmux-sessionizer\n"
 
+# Use catppuccin colors for fzf
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
